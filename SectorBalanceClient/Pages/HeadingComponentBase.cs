97cd0bde113b7144b78f2c8a63f51ebc44f8be2c
gbrooksman <gbrooksman@gmail.com>
2019-06-14 22:11:54 -0400

    svg work

--- SectorBalanceClient/Pages/HeadingComponentBase.cs
+++ SectorBalanceClient/Pages/HeadingComponentBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.RenderTree;
-using SectorBalanceClient.SVG;
+using SectorBalanceClient;
 using Microsoft.AspNetCore.Blazor.Rendering;
 
 namespace SectorBalanceClient
